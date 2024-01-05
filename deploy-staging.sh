15d17333194471d31ecbfae6f23ac597311da2ea
the-oater <hello@oater.de>
2024-01-05 16:21:02 +0100

    adds possibility to deploy to a staging environment

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,7 +1,12 @@
+echo
 echo "Deploying to staging"
+echo
 
+npm install
 npm run build
 
-rsync -cha --progress dist/* oater@hamal.uberspace.de:/home/oater/html/staging
+rsync -cha --progress --delete dist/* oater@oater.de:/home/oater/staging.oater.de/
 
+echo
 echo "Deployed to https://staging.oater.de"
+echo
